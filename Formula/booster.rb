126009ec4efbcddf97b92cb4224adf9e71b66ab2
danielmorandini <danielmorandini@me.com>
2018-05-28 20:01:09 +0200

    Brew formula update for booster version v0.4.0

--- Formula/booster.rb
+++ Formula/booster.rb
@@ -1,9 +1,9 @@
 class Booster < Formula
   desc "peer-to-peer network interface balancer"
   homepage "https://booster.tecnoporto.co/"
-  url "https://github.com/danielmorandini/booster/releases/download/v0.3.4/booster_0.3.4_darwin_64bit.tar.gz"
-  version "0.3.4"
-  sha256 "b6d4970de9d270f90115ca485cc7359bf0dd00773afe3ee54c1784e5a15add1f"
+  url "https://github.com/danielmorandini/booster/releases/download/v0.4.0/booster_0.4.0_darwin_64bit.tar.gz"
+  version "0.4.0"
+  sha256 "f27da6b38086f525bba3fb74e9e4c62da00a660b0a41762b60695b0b40730bb6"
 
   def install
     bin.install "booster"
